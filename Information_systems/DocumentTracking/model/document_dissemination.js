aac820343a078197c8b90482d17a37b36636f5dd
Aljon C. Tocayon <actWhitehat27@gmail.com>
2020-08-17 14:27:59 +0800

    dts: document_dissemination

--- Information_systems/DocumentTracking/model/document_dissemination.js
+++ Information_systems/DocumentTracking/model/document_dissemination.js
@@ -1,9 +1,10 @@
 const db = require("../../../config/Database_config/db");
 const dateTime = require("../../common/Get_CurrentDateTime");
-const document_dissemination = (
-  user_id,
+
+const insert_document_logs = (
+  userId,
   doc_id,
-  doc_info,
+  docInfo,
   remarks,
   destination,
   res
@@ -27,6 +28,72 @@ const document_dissemination = (
 
     res.status(200).send("success");
   });
+};
+
+const insert_document_destination = (
+  userId,
+  doc_id,
+  doc_info,
+  destination,
+  res
+) => {
+  let sql = "";
+  sql += "INSERT INTO documents ";
+  sql += "(documentID, creator, subject, doc_type, ";
+  sql += "note, date_time_created, status, ref, category ) ";
+  sql += "VALUES  ? ";
+  const values = [
+    [
+      doc_id,
+      userId,
+      doc_info.subject,
+      doc_info.docTypeId,
+      doc_info.note,
+      dateTime(),
+      "1",
+      "0",
+      doc_info.category,
+    ],
+  ];
+
+  db().query(sql, [values], function (err, result) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    }
+
+    if (destination.des.length > 1) {
+      for (let i = 0; i < destination.des.length; i++) {
+        insert_document_logs(
+          userId,
+          doc_id,
+          doc_info,
+          destination.remarks,
+          destination.des[i],
+          res
+        );
+      }
+    } else {
+      insert_document_logs(
+        userId,
+        doc_id,
+        doc_info,
+        destination.remarks,
+        destination.des[0],
+        res
+      );
+    }
+  });
+};
+const document_dissemination = (
+  user_id,
+  doc_id,
+  doc_info,
+  remarks,
+  destination,
+  res
+) => {
+  insert_document_destination(user_id, doc_id, doc_info, destination, res);
 };
 
 module.exports = document_dissemination;
