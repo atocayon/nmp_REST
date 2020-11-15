ba15b3de29d2257eb5f9ee27902c65901f2337a6
Aljon C. Tocayon <actwhitehat27@gmail.com>
2020-11-15 20:40:42 +0800

    update: work queue fetch job requests

--- Information_systems/WorkQueue/model/client_job_requests.js
+++ Information_systems/WorkQueue/model/client_job_requests.js
@@ -12,15 +12,41 @@ const client_job_request = (user_id, res) => {
   sql += "a.scope_of_work, ";
   sql += "DATE_FORMAT(a.deadline,'%M %d, %Y ') AS date_needed, ";
   sql += "DATE_FORMAT(a.date,'%M %d, %Y ') AS date_requested, ";
-  sql += " d.section, ";
+  sql += " d.secshort, ";
   sql += "b.name AS inspector, ";
-  sql += "c.status AS status ";
+  sql += "bb.name AS requisitioner, ";
+  sql += "c.status AS status, ";
+  sql += "e.findings, ";
+  sql += "e.recommendations, ";
+  sql += "f.system_interface, ";
+  sql += "f.functional_capabilities, ";
+  sql += "f.data_structure, ";
+  sql += "f.reliability, ";
+  sql += "f.security, ";
+  sql += "f.quality, ";
+  sql += "f.contraints, ";
+  sql += "g.item_no, ";
+  sql += "g.serial_no, ";
+  sql += "g.brand, ";
+  sql += "g.memory_capacity, ";
+  sql += "g.item_model, ";
+  sql += "g.color, ";
+  sql += "g.measurement, ";
+  sql += "g.location, ";
+  sql += "GROUP_CONCAT(h.task) AS type_of_work ";
   sql += "FROM work_queue_task a ";
   sql += "LEFT JOIN users b ON a.inspector = b.user_id ";
+  sql += "LEFT JOIN users bb ON a.requisitioner = bb.user_id ";
   sql +=
     "LEFT JOIN work_queue_logs c ON a.id = c.task_id AND c.id = (SELECT MAX(id) FROM work_queue_logs cc WHERE cc.task_id = c.task_id)";
   sql += "LEFT JOIN sections d ON a.section = d.secid ";
-  sql += "WHERE a.requisitioner = ?";
+  sql += "LEFT JOIN work_queue_inspection e ON a.id = e.task_id ";
+  sql +=
+    "LEFT JOIN work_queue_info_system_specifications f ON a.id = f.task_id ";
+  sql +=
+    "LEFT JOIN work_queue_specification_descriptions g ON a.id = g.task_id ";
+  sql += "LEFT JOIN work_queue_type_of_work h ON a.id = h.task_id ";
+  sql += "WHERE a.requisitioner = ? GROUP BY a.id";
 
   db.query(sql, [parseInt(user_id)], (err, rows, fields) => {
     if (err) {
